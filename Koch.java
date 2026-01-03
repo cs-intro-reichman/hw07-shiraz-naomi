ed61421f004a4a0a0ede05024ebb17034cc014d7
shiraz-naomi <shiraz.ross@post.runi.ac.il>
2026-01-03 16:54:25 +0200

    my wifi crashed

--- Koch.java
+++ Koch.java
@@ -3,40 +3,66 @@ public class Koch {
 
 	public static void main(String[] args) {
 
-		//// Uncomment the first code block to test the curve function.
-		//// Uncomment the second code block to test the snowflake function.
-		//// Uncomment only one block in each test, and remember to compile
-		//// the class whenever you change the test.
-
-        /*
-		// Tests the curve function:
-		// Gets n, x1, y1, x2, y2,
-		// and draws a Koch curve of depth n from (x1,y1) to (x2,y2).
-		curve(Integer.parseInt(args[0]),
-			  Double.parseDouble(args[1]), Double.parseDouble(args[2]), 
-		      Double.parseDouble(args[3]), Double.parseDouble(args[4]));
-		*/
+		//// Uncomment the first code block to test the curve function. Uncomment the
+		/// second code block to test the snowflake function. Uncomment only one block in
+		/// each test, and remember to compile the class whenever you change the test.
 
 		/*
-		// Tests the snowflake function:
-		// Gets n, and draws a Koch snowflake of n edges in the standard canvass.
-		snowFlake(Integer.parseInt(args[0]));
-		*/
+		 * // Tests the curve function:
+		 * // Gets n, x1, y1, x2, y2,
+		 * // and draws a Koch curve of depth n from (x1,y1) to (x2,y2).
+		 * curve(Integer.parseInt(args[0]),
+		 * Double.parseDouble(args[1]), Double.parseDouble(args[2]),
+		 * Double.parseDouble(args[3]), Double.parseDouble(args[4]));
+		 */
+
+		/*
+		 * // Tests the snowflake function:
+		 * // Gets n, and draws a Koch snowflake of n edges in the standard canvass.
+		 * snowFlake(Integer.parseInt(args[0]));
+		 */
 	}
 
-	/** Gets n, x1, y1, x2, y2,
-     *  and draws a Koch curve of depth n from (x1,y1) to (x2,y2). */
+	/**
+	 * Gets n, x1, y1, x2, y2,
+	 * and draws a Koch curve of depth n from (x1,y1) to (x2,y2).
+	 */
 	public static void curve(int n, double x1, double y1, double x2, double y2) {
-		StdDraw.line(x1, y1, x2, y2);
-		//// Write the rest of your code below.
+		if (n == 0) {
+			StdDraw.line(x1, y1, x2, y2);
+			return;
+		}
+
+		double xP1 = x1 + (x2 - x1) / 3.0;
+		double yP1 = y1 + (y2 - y1) / 3.0;
+		double xP2 = x1 + 2.0 * (x2 - x1) / 3.0;
+		double yP2 = y1 + 2.0 * (y2 - y1) / 3.0;
+		double newsqrt = Math.sqrt(3) / 6.0;
+		double xP3 = 0.5 * (x1 + x2) - newsqrt * (y2 - y1);
+		double yP3 = newsqrt * (x2 - x1) + 0.5 * (y1 + y2);
+
+		curve(n - 1, x1, y1, xP2, yP2);
+		curve(n - 1, xP1, yP1, x2, y2);
+		curve(n - 1, xP2, yP2, x2, y2);
+
 	}
 
-    /** Gets n, and draws a Koch snowflake of n edges in the standard canvass. */
+	/** Gets n, and draws a Koch snowflake of n edges in the standard canvass. */
 	public static void snowFlake(int n) {
 		// A little tweak that makes the drawing look better
 		StdDraw.setYscale(0, 1.1);
 		StdDraw.setXscale(0, 1.1);
-		// Draws a Koch snowflake of depth n
-		//// Write the rest of your code below.
+
+		double x1 = 0.2;
+		double y1 = 0.3;
+		double y2 = 0.3;
+		double x2 = 0.9;
+		double x3 = 0.55;
+		double y3 = 0.3 + Math.sqrt(3) * (0.7 / 2.0);
+
+		curve(n, x1, y1, x2, y2);
+		curve(n, x2, y2, x3, y3);
+		curve(n, x3, y3, x1, y1);
+
 	}
 }
--- Sierpinski.java
+++ Sierpinski.java
@@ -1,18 +1,50 @@
 /** Draws ths Sierpinski Triangle fractal. */
 public class Sierpinski {
-	
+
 	public static void main(String[] args) {
 		sierpinski(Integer.parseInt(args[0]));
 	}
-	
+
 	// Draws a Sierpinski triangle of depth n on the standard canvass.
-	public static void sierpinski (int n) {
-		//// Replace this comment with your code
+	public static void sierpinski(int n) {
+		double h = Math.sqrt(3) / 2;
+
+		double x1 = 0;
+		double x2 = 0;
+		double x3 = 1.0 / 2.0;
+		double y1 = 0;
+		double y2 = 0;
+		double y3 = h;
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.line(x1, y1, x2, y2);
+		StdDraw.line(x2, y2, x3, y3);
+		StdDraw.line(x3, y3, x1, y1);
+		sierpinski(n, x1, x2, x3, y1, y2, y3);
+
 	}
-	
+
 	// Does the actual drawing, recursively.
 	private static void sierpinski(int n, double x1, double x2, double x3,
-		                                 double y1, double y2, double y3) {
-		//// Replace this comment with your code
+			double y1, double y2, double y3) {
+		if (n == 0)
+			return;
+
+		double midX1 = (x1 + x2) / 2.0;
+		double midY1 = (y1 + y2) / 2.0;
+
+		double midX2 = (x2 + x3) / 2.0;
+		double midY2 = (y2 + y3) / 2.0;
+
+		double midX3 = (x3 + x1) / 2.0;
+		double midY3 = (y3 + y1) / 2.0;
+
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.line(midX1, midY1, midX2, midY2);
+		StdDraw.line(midX2, midY2, midX3, midY3);
+		StdDraw.line(midX3, midY3, midX1, midY1);
+
+		sierpinski(n - 1, x1, midX2, midX3, y1, midY2, midY3);
+		sierpinski(n - 1, midX1, x2, midX3, midY2, y2, midY3);
+		sierpinski(n - 1, midX1, midX2, x3, midY2, midY2, y3);
 	}
 }
