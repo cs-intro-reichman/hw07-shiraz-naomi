b3aa050e04350cb3aeab943b0a7093b1d7f88b93
shiraz-naomi <shiraz.ross@post.runi.ac.il>
2026-01-04 18:31:36 +0200

    Update Binomial.java

--- Binomial.java
+++ Binomial.java
@@ -20,8 +20,8 @@ public class Binomial {
 	}
 
 	// Computes the Binomial function, efficiently
-	public static int binomial(int n, int k) {
-		int[][] memo = new int[n + 1][k + 1];
+	public static long binomial(int n, int k) {
+		long[][] memo = new long[n + 1][k + 1];
 		for (int i = 0; i <= n; i++) {
 			for (int j = 0; j <= k; j++) {
 				memo[i][j] = -1;
@@ -30,7 +30,7 @@ public class Binomial {
 		return binomial(n, k, memo);
 	}
 
-	private static int binomial(int n, int k, int[][] memo) {
+	private static long binomial(int n, int k, long[][] memo) {
 		if (memo[n][k] != -1) {
 			return memo[n][k];
 		}
